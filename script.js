2ac6d7264461afa24e1558bb61ec0ef1ddd277ac
olaprish <prishpon@gmail.com>
2020-03-23 11:17:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let arr = ['Apple','peach','Cherry','Bananas'];
+let cat = {
+    kind:"british",
+    color:"orange"
+}
+//let question = +4 + "ff";
+//alert(typeof(question))
+
+let num = 50;
+
+switch(num){
+    case num < 40:
+    alert("no");
+    break;
+    case num > 40:
+    alert("NOOOO");
+    break;
+    case num == 50:
+    alert("O yeath!");
+    break;  
+}
